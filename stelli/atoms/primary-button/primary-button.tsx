eaf9a43b818ed86940a9e889ec88a524db1e0600
Samuel Silva <samuelsilvawb@gmail.com>
2023-04-10 16:16:42 +0100

    feat: Add more icons on primary button

--- stelli/atoms/primary-button/primary-button.tsx
+++ stelli/atoms/primary-button/primary-button.tsx
@@ -2,15 +2,41 @@ import React, { ReactNode } from "react";
 import styles from "./primary-button.module.css";
 import { Minus } from "@stelli/components-library-experiment.icons.minus";
 import { Help } from "@stelli/components-library-experiment.icons.help";
+import { Alert } from "@stelli/components-library-experiment.icons.alert";
+import { Bell } from "@stelli/components-library-experiment.icons.bell";
+import { Book } from "@stelli/components-library-experiment.icons.book";
+import { Calendar } from "@stelli/components-library-experiment.icons.calendar";
+import { Camera } from "@stelli/components-library-experiment.icons.camera";
+import { Card } from "@stelli/components-library-experiment.icons.card";
+import { Cart } from "@stelli/components-library-experiment.icons.cart";
+import { Clock } from "@stelli/components-library-experiment.icons.clock";
 
 export type PrimaryButtonProps = {
   children?: ReactNode;
-  iconName: "minus" | "help";
+  iconName:
+    | "minus"
+    | "help"
+    | "alert"
+    | "bell"
+    | "book"
+    | "calendar"
+    | "camera"
+    | "card"
+    | "cart"
+    | "clock";
 };
 
 const iconsMap = {
   minus: Minus,
   help: Help,
+  alert: Alert,
+  bell: Bell,
+  book: Book,
+  calendar: Calendar,
+  camera: Camera,
+  card: Card,
+  cart: Cart,
+  clock: Clock,
 };
 
 export function PrimaryButton({ children, iconName }: PrimaryButtonProps) {
