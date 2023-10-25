0c2bfd2e73f4853541e9634d666a4c25bc5f6508
Sergio Vanacloig <sergiovanacloig97@gmail.com>
2023-10-25 12:20:58 +0200

    Fix primary button composition error

--- stelli/atoms/primary-button/primary-button.composition.tsx
+++ stelli/atoms/primary-button/primary-button.composition.tsx
@@ -2,5 +2,6 @@ import React from "react";
 import { PrimaryButton } from "./primary-button";
 
 export const BasicPrimaryButton = () => {
-  return <PrimaryButton iconName="help">hello world!</PrimaryButton>;
+  // Passing div instead of a real Icon just to avoid creating a dependency for only the composition
+  return <PrimaryButton icon={<div>Icon</div>}>hello world!</PrimaryButton>;
 };
